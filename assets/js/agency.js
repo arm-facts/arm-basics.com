797055f6b05ea39d58b9fe2eba759ce38691b70c
arm-facts <41009579+arm-facts@users.noreply.github.com>
2018-07-12 22:54:43 +0800

    add down counter, website will be closed in July 15 2018 23:59:59, London time.

--- assets/js/agency.js
+++ assets/js/agency.js
@@ -29,5 +29,39 @@
             top: 100
         }
     })
+    function getTimeRemaining(endtime) {
+        var t = Date.parse(endtime) - Date.parse(new Date());
+        var seconds = Math.floor((t / 1000) % 60);
+        var minutes = Math.floor((t / 1000 / 60) % 60);
+        var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+        var days = Math.floor(t / (1000 * 60 * 60 * 24));
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+    
+    function initializeClock(id, endtime) {
+        var clock = document.getElementById(id);
+        var downCounterSpan = clock.querySelector('.downcounter');
+     
+        function updateClock() {
+            var t = getTimeRemaining(endtime);
+           
+            downCounterSpan.innerHTML = "This website will be closed in " + t.days + " Days " + ('0' + t.hours).slice(-2) + " Hours " + ('0' + t.minutes).slice(-2) + " Minutes " + ('0' + t.seconds).slice(-2) + " Seconds.";
+           
+            if (t.total <= 0) {
+              clearInterval(timeinterval);
+            }
+        }
+     
+        updateClock();
+        var timeinterval = setInterval(updateClock, 1000);
+    }
+    var deadline = 'July 15 2018 23:59:59 GMT+0100';
+    initializeClock('clockdiv', deadline);
 
 })(jQuery); // End of use strict
